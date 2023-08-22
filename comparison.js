a5596b916ac42ea2e912f72ad518c2ac2e24e7fa
user.email <jobayer.ahmed109@gmail.com>
2023-08-22 22:07:57 +0600

    if condition explored

---
+++ comparison.js
@@ -0,0 +1,17 @@
+// if condition
+const num = 11;
+
+if (num % 2 === 0) {
+  console.log(`11 is a even number `);
+} else {
+  console.log(`11 is a odd number `);
+}
+
+// Syntax of IF condition:
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
