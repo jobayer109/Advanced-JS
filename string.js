4a2e9b8fea9623a8acfb5e109693e3e6f8687bbf
user.email <jobayer.ahmed109@gmail.com>
2023-08-22 11:49:47 +0600

    string explored

---
+++ string.js
@@ -0,0 +1,11 @@
+var x = "This is text";
+var y = "This is text";
+var z = `This is another text`;
+
+var text1 = String(`Hello`);
+var text2 = String("Gello");
+var text3 = String("1010");
+var text4 = String(100);
+var text5 = 100;
+
+console.log(text1, text2, text3, text4, text5);
