5de31ce8fcb8210360b352f9cab2a0ba70e1ab5d
user.email <jobayer.ahmed109@gmail.com>
2023-08-23 12:16:10 +0600

    Do-while loop is explored

---
+++ doWhile.js
@@ -0,0 +1,13 @@
+// While loop syntax
+
+let isRunning = false;
+while (isRunning) {
+  console.log("I am running from while loop");
+}
+
+//
+// Do-while loop syntax:
+do {
+  console.log("I am running from Do-while loop");
+  //   isRunning = true;  // No need this  if the condition is false
+} while (isRunning);
