cff8573d9b6701c9c0fbcfff836ea65fe22624f0
user.email <jobayer.ahmed109@gmail.com>
2023-08-23 08:30:42 +0600

    Basic For loop explored

---
+++ forLoop.js
@@ -0,0 +1,20 @@
+// Odd number calculation
+for (let i = 0; i < 10; i++) {
+  if (i % 2 !== 0) {
+    // console.log(i);
+  }
+}
+
+//
+//  Even number calculation
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) {
+    // console.log(i);
+  }
+}
+
+//
+//
+// Operator operation
+let x = 5;
+console.log((x += 6));
