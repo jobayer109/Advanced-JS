087435221010a01786c13c4924e06c4c0032facc
user.email <jobayer.ahmed109@gmail.com>
2023-08-26 07:34:34 +0600

    Array methods are explored

--- Array/insertRemove.js
+++ Array/insertRemove.js
@@ -1,20 +1,3 @@
-/*
-    Array methods:
-    ------------------>
-        Array length
-        Array toString()
-        Array pop() 
-        Array push()
-        Array shift()
-        Array unshift()
-        Array join()
-        Array delete()
-        Array concat()
-        Array flat()
-        Array splice()
-        Array slice()
-*/
-
 //
 //
 const arr = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -34,6 +17,7 @@ arr.toString(); //  1,2,3,4,5,6,7,8
 */
 arr.splice(3, 0, 11); //  [  1, 2, 3, 11, 4,  5, 6, 7,  8 ]
 arr.splice(3, 2, 11); //  [  1, 2, 3, 11, 5, 6, 7,  8 ]
+arr.splice(3, 0, 11, 17, 15, 1, 8); //  [  1, 2, 3, 11,  17, 15, 1, 8, 4, 5, 6, 7,  8 ]
 // console.log(arr);
 
 //
---
+++ Array/methods.js
@@ -0,0 +1,93 @@
+/*
+    Array methods:
+    ------------------>
+        Array length
+        Array toString()
+        Array pop() 
+        Array push()
+        Array shift()
+        Array unshift()
+        Array join()
+        Array delete()
+        Array concat()
+        Array flat()
+        Array splice()
+        Array slice()
+*/
+
+//
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// Length
+let lengths = fruits.length; // 4
+
+// --------------------------------------------------------------------->
+
+// toString()
+let toString = fruits.toString(); // Banana,Orange,Apple,Mango
+
+// --------------------------------------------------------------------->
+
+// Join()
+let joined1 = fruits.join(""); // BananaOrangeAppleMango
+let joined2 = fruits.join(" "); // Banana Orange Apple Mango
+let joined3 = fruits.join(" * "); // Banana * Orange * Apple * Mango
+
+// --------------------------------------------------------------------->
+
+// Delete()
+// Using delete leaves undefined holes in the array.
+let deleted = delete fruits[1]; // [ 'Banana', <1 empty item>, 'Apple', 'Mango' ]
+
+// --------------------------------------------------------------------->
+
+// concat()
+// The concat() method creates a new array.
+// The concat() method can take any number of array arguments.
+// The concat() method can also take strings as arguments
+// Example: 1
+let boys = ["Ahmed", "Joshim"];
+let girls = ["Taima", "Karina"];
+let concatenated = boys.concat(girls); // [ 'Ahmed', 'Joshim', 'Taima',  'Karina' ]
+
+// Example: 2
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin", "Morgan"];
+const myChildren = arr1.concat(arr2, arr3);
+
+// Example: 3
+const arr4 = ["Emil", "Tobias", "Linus"];
+const childrens = arr4.concat("Peter"); // [ 'Emil', 'Tobias', 'Linus', 'Peter' ]
+
+// --------------------------------------------------------------------->
+
+// flat()
+const myArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8, [18, 15]],
+];
+const flatted = myArr.flat(); // [ 1, 2, 3, 4, 5, 6, 7, 8, [ 18, 15 ] ]
+const flattedMore = flatted.flat(); // [1, 2, 3, 4, 5, 6, 7, 8, 18, 15];
+
+// --------------------------------------------------------------------->
+
+// splice()
+// We talk about this in insertRemove.js file.
+
+// --------------------------------------------------------------------->
+
+// slice() method
+// The slice() method creates a new array.
+// The slice() method does not remove any elements from the source array.
+// If the end argument is omitted, like in the first examples, the slice() method slices out the rest of the array.
+
+const fevFruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const sliced1 = fevFruits.slice(2); // [ 'Lemon', 'Apple', 'Mango' ]
+const sliced2 = fevFruits.slice(1, 4); // [ 'Orange', 'Lemon', 'Apple' ]
+
+// --------------------------------------------------------------------->
+
+// Finding Max and Min Values in an Array:
