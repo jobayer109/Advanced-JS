1517151678f77ace3cc61ae1daba00191821c485
user.email <jobayer.ahmed109@gmail.com>
2023-08-30 22:53:36 +0600

    some noted are taken about functional programming

--- Functional_Programming/intro.js
+++ Functional_Programming/intro.js
@@ -1,19 +1,42 @@
-// Functional programming with Javascript.
+/*
 
-//  ----> Function acts like a value (Definition)
 
-// -------------------------------------------------------------------------------->>
+Q: What is functional programming ?
 
-// What is functional programming ?
-`Functional programming is programming paradigm- a style of building the structure and  
- element of computer program that treats computation as the evaluation of mathematical
- functions and avoid  changing-state and mutable data `;
+    Functional programming is programming paradigm - a style of building the structure 
+    and element of computer program that treats computation as the evaluation of 
+    mathematical functions and avoid  changing-state and mutable data. 
 
-// -------------------------------------------------------------------------------->>
+    Or,
+    Functional programming is a programming paradigm in which we try to bind everything 
+    in pure mathematical functions style. It is a declarative type of programming style. 
+    Its main focus is on “what to solve” in contrast to an imperative style where the 
+    main focus is “how to solve”.
 
-// Three main terms:
-`     1. Pure function.     `;
-`     2. First class  function     `;
-`     2. Higher order  function    `;
+    
+    ----------------------------------------------------------------------------------------------------
 
-// -------------------------------------------------------------------------------->>
+    Three main terms:
+        1. Pure function.
+        2. First class  function
+        3. Higher order  function
+
+     
+     ---->> Function acts like a value (Definition)
+
+    ----------------------------------------------------------------------------------------------------
+    
+    Notes:
+        * It uses expressions instead of statements. An expression is evaluated to
+          produce a value; whereas a statement is executed to assign variables. 
+        * Functional Programming is based on Lambda Calculus: 
+          Lambda calculus is a framework developed by Alonzo Church to study computations with functions.
+          It can be called as the smallest programming language in the world.
+
+
+    Read the GeeksForGeeks documentation
+
+
+
+
+*/
