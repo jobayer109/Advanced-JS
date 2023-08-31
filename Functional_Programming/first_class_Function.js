807fab4bacd1f53d809aaf96f7b772e068e58fb0
user.email <jobayer.ahmed109@gmail.com>
2023-08-31 23:21:14 +0600

    First class function definition

---
+++ Functional_Programming/first_class_Function.js
@@ -0,0 +1,3 @@
+/*
+First-Class Function: A programming language is said to have First-class functions if functions in that language are treated like other variables. So the functions can be assigned to any other variable or passed as an argument or can be returned by another function. JavaScript treat function as a first-class-citizens
+*/
