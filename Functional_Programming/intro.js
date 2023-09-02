fa6957d63e0ad18af622b61e83a9f3d10cad8c30
user.email <jobayer.ahmed109@gmail.com>
2023-09-02 20:35:02 +0600

    Pure function with example

--- Functional_Programming/intro.js
+++ Functional_Programming/intro.js
@@ -3,9 +3,10 @@
 
 Q: What is functional programming ?
 
-    Functional programming is programming paradigm - a style of building the structure 
-    and element of computer program that treats computation as the evaluation of 
-    mathematical functions and avoid  changing-state and mutable data. 
+    Functional programming is programming paradigm (স্বর্গ) - where a style of building 
+    the structure and element of computer program that treats computation as the 
+    evaluation of mathematical functions and avoid  changing-state and mutable 
+    (পরিবর্তনযোগ্য) data.
 
     Or,
     Functional programming is a programming paradigm in which we try to bind everything 
@@ -14,7 +15,7 @@ Q: What is functional programming ?
     main focus is “how to solve”.
 
     
-    ----------------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------
 
     Three main terms:
         1. Pure function.
@@ -24,21 +25,22 @@ Q: What is functional programming ?
      
      ---->> Function acts like a value (Definition)
 
-    ----------------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------
     
     Notes:
         * It uses expressions instead of statements. An expression is evaluated to
           produce a value; whereas a statement is executed to assign variables. 
         * Functional Programming is based on Lambda Calculus: 
-          Lambda calculus is a framework developed by Alonzo Church to study computations with functions.
-          It can be called as the smallest programming language in the world.
+          Lambda calculus is a framework developed by Alonzo Church to study 
+          computations with functions.
+        * It can be called as the smallest programming language in the world.
 
 
     Read the GeeksForGeeks documentation
 
 
 
-    --------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 
 
 
--- Functional_Programming/pure_Function.js
+++ Functional_Programming/pure_Function.js
@@ -1,19 +1,35 @@
 /*
+   Topics:
+      1. Pure function definition and characteristics.
+      2. Some not a pur function example
+      
 
-Pure functions: These functions have two main properties. First, they always produce the same output for same arguments irrespective of anything else. 
-Secondly, they have no side-effects i.e. they do not modify any arguments or local/global variables or input/output streams. 
-Later property is called immutability. The pure function’s only result is the value it returns. They are deterministic. 
-Programs done using functional programming are easy to debug because pure functions have no side effects or hidden I/O. Pure functions also make it easier to write parallel/concurrent applications. When the code is written in this style, a smart compiler can do many things – it can parallelize the instructions, wait to evaluate results when needing them, and memorize the results since the results never change as long as the input doesn’t change. 
-example of the pure function: 
+        1. Pure function.
+        ----------------->
+            * Pure function always produce the same output for same arguments 
+              irrespective of anything else.  
 
+            * It's easy to debug because pure functions have no side effects 
+              or hidden I/O.
+
+            * Without arguments a function can be pure function.
+
+            * Pure functions also make it easier to write parallel/concurrent 
+              applications.When the code is written in this style, a smart compiler can 
+              do many things – it can parallelize the instructions, wait to evaluate 
+              results when needing them, and memorize the results since the results 
+              never change as long as the input doesn’t change.
 */
 
-sum(x, y); // sum is function taking x and y as arguments
-return x + y; // sum is returning sum of x and y without changing them
+// ----------------------------------------------------------------------------------->>
 
-fib(n);
-if (n <= 1) return 1;
-else return fib(n - 1) + fib(n - 2);
+// Pure function definition and characteristics.
 
-x = x + 1; // this changes the value assigned to the variable x.
-// So the expression is not referentially transparent.
+// Example: 1
+function sqr(n) {
+  return n * n;
+}
+sqr(2); // 4; It alway return same result without side effect.
+sqr(3); // 9; It alway return same result without side effect.
+
+// ----------------------------------------------------------------------------------->>
