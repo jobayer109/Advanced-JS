bda906729aa88cf827d0948f4a59583056b1deff
user.email <jobayer.ahmed109@gmail.com>
2023-09-02 22:30:39 +0600

    Return function from another function

--- Functional_Programming/higher_order.js
+++ Functional_Programming/higher_order.js
@@ -16,3 +16,18 @@ function multiply(a, b, func) {
   return a + b * m;
 }
 multiply(3, 6, add); // 45
+
+// ----------------------------------------------------------------------------------->>
+
+// 2. We can return function from another function.
+
+function outerFunction() {
+  function innerFunction() {
+    console.log("This is the inner function.");
+  }
+
+  return innerFunction;
+}
+const returnedFunction = outerFunction();
+
+returnedFunction(); // This will log "This is the inner function."
