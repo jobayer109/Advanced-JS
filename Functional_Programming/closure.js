c320a468484c2262b46c1fb64af0ded07da2ac17
user.email <jobayer.ahmed109@gmail.com>
2023-09-03 20:47:18 +0600

    some imp notes about console.log and console.dir

--- Functional_Programming/closure.js
+++ Functional_Programming/closure.js
@@ -1,6 +1,11 @@
 /*
    Notes:
         * Jodi kono scope a baire theke data ase take closure bole.
+        * The name "console.dir" is derived from its primary function, which is to 
+          display the directory or structure of an object.
+        * console.log is used for basic text-based logging, while console.dir is used 
+          for exploring and inspecting the structure of JavaScript objects in a more 
+          detailed and interactive manner. 
        
 */
 
