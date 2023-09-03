2b05196fb9e29f76a0ce6f65af6ed7f9dbbb0505
user.email <jobayer.ahmed109@gmail.com>
2023-09-03 22:08:40 +0600

    Best practice of callback function without code duplication

--- Functional_Programming/callabck.js
+++ Functional_Programming/callabck.js
@@ -12,15 +12,46 @@ function sample(a, b) {
   let c = a + b;
   let d = a - b;
 
-  let result = sum(c, d);
-  // this code is fixed for only one function 'sum()'. If I want to subtraction,        multiply, division, modulus; I have to declare same code for different different function which create code duplication. This is bad practice.
+  let result = (c, d);
+  // this code is fixed for only one function 'c()'. If I want to subtraction,        multiply, division, modulus; I have to declare same code for different different function which create code duplication. This is bad practice.
 
   return result;
 }
 
-function sum(a, b) {
+function c(a, b) {
   return a + b;
 }
 
 let result = sample(5, 8);
 result; // 10;
+
+// ----------------------------------------------------------------------------------->>
+
+// 2. Callback function best practice.
+
+function sampleFunc(a, b, callback) {
+  let c = a + b;
+  let d = a - b;
+
+  let result = callback(c, d);
+  return result;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+const result1 = sampleFunc(5, 8, add); // passed a previously defined function
+result1; // 10
+
+const result2 = sampleFunc(5, 8, function (c, d) {
+  // Pass a function directly for subtraction
+  return c - d;
+});
+result2; // 16
+
+const result3 = sampleFunc(10, 8, function (c, d) {
+  // Pass a function directly for subtraction
+  return c * d;
+});
+result3; // 36
