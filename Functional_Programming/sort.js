5b0c7e70c50d0012469deda610f1dbe7aa163507
user.email <jobayer.ahmed109@gmail.com>
2023-09-04 21:08:57 +0600

    Array of object sorting to asc & dsc order

--- Functional_Programming/sort.js
+++ Functional_Programming/sort.js
@@ -34,3 +34,43 @@ arr.sort(function (a, b) {
 arr; // [  0, 1, 4, 5, 5, 5, 6, 7, 8, 9, 9 ]
 
 // ----------------------------------------------------------------------------------->>
+
+// Array of object sorting with callback function.
+
+const persons = [
+  {
+    name: "A",
+    age: 19,
+  },
+  {
+    name: "B",
+    age: 45,
+  },
+  {
+    name: "C",
+    age: 12,
+  },
+  {
+    name: "D",
+    age: 23,
+  },
+];
+
+persons.sort(function (a, b) {
+  if (a.age > b.age) {
+    return 1; // if "-1", it will return to descending order
+  } else if (a.age < b.age) {
+    return -1; // if "1", it will return to descending order
+  } else {
+    return 0;
+  }
+});
+
+persons;
+/* [  
+    { name: "C", age: 12 },  
+    { name: "A", age: 19 },  
+    { name: "D", age: 23 },  
+    { name: "B", age: 45 },];
+*/
+// ----------------------------------------------------------------------------------->>
