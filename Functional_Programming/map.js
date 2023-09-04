53510524ad16be8ab16e161296e1b05aefb3720a
user.email <jobayer.ahmed109@gmail.com>
2023-09-04 06:35:56 +0600

    Custom implementation of map method

--- Functional_Programming/map.js
+++ Functional_Programming/map.js
@@ -17,8 +17,34 @@ arr.map(function (value, index, arr) {
   //   console.log(value, index, arr);
   sum += value;
 });
+
 sum; // 6
 
 // ----------------------------------------------------------------------------------->>
 
 // 2. Custom implementation of array traverse as same as map() method.
+// map() return a new array without evaluating the main array.
+
+function map(arr, callback) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let temp = callback(arr[i], i, arr);
+    newArr.push(temp);
+  }
+  return newArr;
+}
+const add = map(arr, function (value, index, arr) {
+  return value + 5; // [ 6, 7, 8 ]
+});
+
+const multiply = map(arr, function (value) {
+  return value * value; // [ 1, 4, 9 ]
+});
+
+const mTen = map(arr, function (value) {
+  return value * 10; // [ 10, 20, 30 ]
+});
+
+add; // [ 6, 7, 8 ]
+multiply; // [ 1, 4, 9 ]
+mTen; // [10, 20, 30];
