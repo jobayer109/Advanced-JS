94ffd5c46b7d46ddf67a894e0b76dbfde28fdf29
user.email <jobayer.ahmed109@gmail.com>
2023-09-05 15:11:57 +0600

    Taken notes about currying

---
+++ Functional_Programming/currying.js
@@ -0,0 +1,20 @@
+/*
+   Topics:
+        A. Currying function 
+
+    Notes:
+        * Using it we can control arguments passing throw a function.
+        * It use in production level to deal multiple arguments.
+        * No built in tool and software on it.
+*/
+
+// ----------------------------------------------------------------------------------->>
+
+// Traditional addition of numbers.
+
+function add(a, b, c) {
+  return a + b + b;
+}
+add(5, 5, 5); // 15
+
+// ----------------------------------------------------------------------------------->>
