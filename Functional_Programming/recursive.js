d8237a36f782f8248ad547c510639d0ed27b9c83
user.email <jobayer.ahmed109@gmail.com>
2023-09-05 14:52:41 +0600

    Sum of any numbers by recursive

--- Functional_Programming/recursive.js
+++ Functional_Programming/recursive.js
@@ -2,13 +2,14 @@
    Topics:
         A. Recursive function 
         B. Print a code 10 times.
-        C. 
+        C. Sum of any numbers.
         D. 
 
     Notes:
         * Je function nijei nijeke call kore and  loop er moto behave kore. But     
           terminate point / end position bole dite hoy. It's mandatory. otherwise it 
-          work on infinity time.  
+          work on infinity time.
+        * we can do all operation as same as loop.
         * It's a expensive function for machine memory.
         * It's can be use for advance programming and we should work on it.
 
@@ -25,3 +26,15 @@ function sayHi10Times(n) {
   sayHi10Times(n - 1); // Recursive function
 }
 sayHi10Times(10);
+
+// ----------------------------------------------------------------------------------->>
+
+// Sum of any numbers
+function sum(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n + sum(n - 1);
+}
+const result = sum(5);
+result; // 15
