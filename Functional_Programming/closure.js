b06b634972979d1c186bf4cf0f57eca3b05f28b7
user.email <jobayer.ahmed109@gmail.com>
2023-09-06 21:47:57 +0600

    Explanation of closure definition

--- Functional_Programming/closure.js
+++ Functional_Programming/closure.js
@@ -1,16 +1,51 @@
 /*
    Notes:
+        * Closure is when a function is able to remember and access it's lexical 
+          scope, Even when that function executing outside it's lexical scope.
+
+    * ---->> Able to remember and access it's lexical scope.
+    * ---->> Function executing outside it's lexical scope.
+
+
         * Jodi kono scope a baire theke data ase take closure bole.
        
         * The name "console.dir" is derived from its primary function, which is to 
           display the directory or structure of an object.
-        * console.log is used for basic text-based logging, while console.dir is used 
-          for exploring and inspecting the structure of JavaScript objects in a more 
-          detailed and interactive manner. 
+        * console.log is used for basic text-based logging, while console.dir is  
+          used for exploring and inspecting the structure of JavaScript objects in a 
+          more detailed and interactive manner. 
        
 */
 
-// ----------------------------------------------------------------------------------->>
+//--------------------------------------------------------------------------------->>
+
+// Able to remember and access it's lexical scope.
+
+function test() {
+  var testVar = `I'm from lexical scope and closure`;
+
+  function innerFunc() {
+    console.log(testVar);
+  }
+  innerFunc();
+}
+
+//
+// Function executing outside it's lexical scope.
+
+function test1() {
+  var testVar1 = `I'm from lexical scope and closure`;
+
+  return function () {
+    console.log(testVar1);
+  };
+}
+const result1 = test1();
+console.log(result1()); // executing outside it's lexical scope
+
+//--------------------------------------------------------------------------------->>
+
+// Simple look of closure.
 
 let a = "Global scope";
 
@@ -23,4 +58,4 @@ function func() {
 }
 
 const result = func();
-console.dir(result); // Run this code in chrome dev tool for better understand.
+console.dir(result()); // Run this code in chrome dev tool for better understand.
