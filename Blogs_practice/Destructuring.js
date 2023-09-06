c0f93b9ee1fa65db6dec183ef05227bb47cc878a
user.email <jobayer.ahmed109@gmail.com>
2023-09-07 05:21:42 +0600

    Array and object destructuring

---
+++ Blogs_practice/Destructuring.js
@@ -0,0 +1,13 @@
+// Array destructure
+const myArray = [1, 2, 3];
+const [first, second, jobayer] = myArray;
+first; // 1
+second; // 2
+jobayer; // 3
+
+//
+// Object destructure
+const myObject = { x: 1, y: 2, z: 3 };
+const { x, y } = myObject;
+x; // 1
+y; // 2
