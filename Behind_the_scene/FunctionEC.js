a5f862346aa676233d5ed23bd73c718e17ecff3c
user.email <jobayer.ahmed109@gmail.com>
2023-09-06 07:36:17 +0600

    Function execution context

---
+++ Behind_the_scene/FunctionEC.js
@@ -0,0 +1,17 @@
+abc();
+
+function abc() {
+  console.log("I'm calling from EC");
+}
+abc();
+
+// Explanation of the code above.
+/*
+    Creational phase:
+        abc = ref
+        
+
+    Execution phase:
+        When the function called it try to search it's ref and execute.
+
+*/
