1912584c6652a243fb8821db198f04bf8296bf0f
user.email <jobayer.ahmed109@gmail.com>
2023-09-06 07:06:19 +0600

    Two phases of Execution context: Creational phase. and Execution phase.

---
+++ Behind_the_scene/Exicution_context.js
@@ -0,0 +1,36 @@
+/*
+
+    Two phases of Execution context:
+            A. Creational phase.
+            B. Execution phase.
+
+     A. Creational phase:
+     --------------------
+        * Sob global variable, sob dhoroner declaration k ekta 'global variable object'- 
+          er vitore store kore.
+        * Variable er declaration and function, array, object esober ref create kore.
+        * Function er vitore kono code run korar thakle sekhaneo ekoi kaj hoy. initial 
+          phase and execution phase duitai hoy.
+
+
+     B. Execution phase:
+     --------------------
+        * Creational phase create howar por execution phase er kaj suru hoy.
+        * Stack er moddhe ei process ti complete hoy.
+        * Stack a ei kaj ti 'LIFO' Last-In-First-Out principle follow kore.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
