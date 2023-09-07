371c820736a312dad11a5ecaedc210252f59d8b7
user.email <jobayer.ahmed109@gmail.com>
2023-09-07 22:47:19 +0600

    Different option of reduce() method.

--- Functional_Programming/reduce.js
+++ Functional_Programming/reduce.js
@@ -3,6 +3,12 @@
         1. Array traverse using reduce() method.
         2. Custom implementation of array traverse as same as reduce() method.
 
+    Note that the function takes 4 arguments:
+        * The total (the initial value / previously returned value)
+        * The item value
+        * The item index
+        * The array itself
+
     Notes:
         * forEach() can replace the original array element.
         * map() return a new array without evaluating the main array.
@@ -16,9 +22,9 @@ const arr = [1, 2, 3, 4, 5, 6];
 
 const sum = arr.reduce(function (prev, current) {
   return prev + current;
-}, 100); // Initial value is optional
+}); // Initial value is optional
 
-sum; // 121
+sum; // 21
 
 //----------------------<<
 const max = arr.reduce(function (prev, current) {
@@ -26,6 +32,17 @@ const max = arr.reduce(function (prev, current) {
 });
 max; // 6
 
-// ----------------------------------------------------------------------------------->>
+//---------------------------------------------------------------------------->>
+// Different option of reduce() method.
+const numbers = [45, 4, 9, 16, 25];
+
+let sum1 = numbers.reduce(myFunction);
+
+function myFunction(total, value, index, array) {
+  return total + value;
+}
+sum1; // 99
+
+//---------------------------------------------------------------------------->>
 
 // 2. Custom implementation of reduce() method.
