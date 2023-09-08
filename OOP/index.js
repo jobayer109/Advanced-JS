51413face5034d89ef8a58574d1232192ef43896
user.email <jobayer.ahmed109@gmail.com>
2023-09-08 14:10:16 +0600

    Object literal explored

--- OOP/index.js
+++ OOP/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+// Object literal
+const rect = {
+  width: 50,
+  height: 100,
+  draw: function () {
+    `I am a Rectangle`;
+    "Width: " + this.width;
+    "Height: " + this.height;
+  },
+};
+rect.draw();
+rect.height = 120;
+rect.draw();
