4387405d7c9caf0c82722e8fd255d633ed962b29
user.email <jobayer.ahmed109@gmail.com>
2023-09-08 16:37:44 +0600

    Factory pattern of  OOP to avoid code duplication

--- OOP/index.js
+++ OOP/index.js
@@ -1,8 +1,11 @@
 /*
     Topics:
     -------
-        * Object literals
-        * 'this' keyword.
+        A. Object literals
+        B. 'this' keyword's use case.
+        C. Patterns 
+            1. Factory pattern 
+            2. Constructor pattern (Class)
 
 
     Notes:
@@ -16,21 +19,42 @@ const rect = {
   width: 50,
   height: 100,
   draw: function () {
-    console.log(`I am a Rectangle`);
+    // console.log(`I am a Rectangle`);
     this.properties();
   },
   properties: function () {
-    console.log("Width: " + this.width);
-    console.log("Height: " + this.height);
+    // console.log("Width: " + this.width);
+    // console.log("Height: " + this.height);
   },
 };
 rect.draw();
 
 // If I want to print 'properties' method from another function, then ----->>
-
 const another = {
   width: 45,
   height: 78,
   print: rect.properties, // Properties method print current object's width and height with the help of properties method's 'this' keyword
 };
 another.print();
+
+// --------------------------------------------------------------------------->>
+
+// Factory pattern
+
+function factoryRect(width, height) {
+  return {
+    width: width,
+    height: height,
+    draw: function () {
+      console.log(`I am a Factory pattern`);
+      this.properties();
+    },
+    properties: function () {
+      console.log("Width: " + this.width);
+      console.log("Height: " + this.height);
+    },
+  };
+}
+
+const res1 = factoryRect(100, 80);
+res1.draw();
