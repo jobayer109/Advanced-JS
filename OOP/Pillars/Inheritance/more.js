9adb4a4f1eefa0758ea9ea1c80e57a6c3aa53ad1
user.email <jobayer.ahmed109@gmail.com>
2023-09-11 06:19:56 +0600

    Took note on Iterate / Traverse Object to access keys

---
+++ OOP/Pillars/Inheritance/more.js
@@ -0,0 +1,40 @@
+/*
+    Topics:
+    ----------------------------------------
+        A. Iterate / Traverse Object and HasOwnProperty method. 
+        B. 
+        C. 
+        D. 
+        E. 
+        F.  
+
+
+    Notes:
+    ------
+        A. Iterate / Traverse Object and HasOwnProperty method:
+        -------------------------------------------------------
+          1. To get only "keys" of the object, it returns only properties name of 
+             Instance cell, not prototype cell property.
+             Ex: Object.keys(sqr1) 
+
+          2. To get all properties name of an object we should apply "for-in" loop. It 
+             returns all properties of Instance and Prototype member cell.
+
+          
+          
+        B. 
+        ----------------------------------
+          *  
+
+
+        C.
+        -----------------------
+          * 
+
+
+
+*/
+
+// ------------------------------------------------------------------------->>
+
+// -----------------------------------------------------------------------------<<
