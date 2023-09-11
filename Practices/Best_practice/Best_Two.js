2465f6882963c3e586a9bb2f0641ae86409beaab
user.email <jobayer.ahmed109@gmail.com>
2023-09-11 14:20:31 +0600

    null check before accessing a variable or key in object

---
+++ Practices/Best_practice/Best_Two.js
@@ -0,0 +1,37 @@
+/*
+    Topics:
+    ----------------------------------------
+        A. null check before accessing a variable or key in object.
+        B. 
+        C. 
+        D. 
+        E. 
+        F.  
+
+
+    Notes:
+    ------
+        A. 
+        -------------------------------------------------------
+          * 
+         
+*/
+
+// ----------------------------------------------------------------------------->>
+
+// A. null check before accessing a variable or key in object.
+// Way-1: using ternary operator.
+const getNameInUpperCase = (name) => {
+  return name ? name.toUpperCase() : "";
+};
+const result = getNameInUpperCase("Jobayer");
+const result1 = getNameInUpperCase();
+// console.log(result, result1);
+
+// Way-2: Using OR || operator.
+const getNameInLowerCase = (name) => {
+  return (name || "").toLowerCase();
+};
+const result2 = getNameInLowerCase("JOBAYER");
+const result3 = getNameInLowerCase();
+// console.log(result2, result3);
