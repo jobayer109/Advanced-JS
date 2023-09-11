3ffba478b08620d488d12a6d73252a8ea19d27da
user.email <jobayer.ahmed109@gmail.com>
2023-09-11 07:03:41 +0600

    Don't Overwrite Built in Prototypes

--- OOP/Pillars/Inheritance/more.js
+++ OOP/Pillars/Inheritance/more.js
@@ -2,7 +2,7 @@
     Topics:
     ----------------------------------------
         A. Iterate / Traverse Object and HasOwnProperty method. 
-        B. 
+        B. Don't Overwrite Built in Prototypes
         C. 
         D. 
         E. 
@@ -13,20 +13,23 @@
     ------
         A. Iterate / Traverse Object and HasOwnProperty method:
         -------------------------------------------------------
-          1. To get only "keys" of the object, it returns only properties name of 
+          * To get only "keys" of the object, it returns only properties name of 
              Instance cell, not prototype cell property.
              Ex: Object.keys(sqr1) 
 
-          2. To get all properties name of an object we should apply "for-in" loop. It 
+          * To get all properties name of an object we should apply "for-in" loop. It 
              returns all properties of Instance and Prototype member cell.
 
-          3. By using "hasOwnProperty" method, we can confirm that the property is existed 
+          * By using "hasOwnProperty" method, we can confirm that the property is existed 
              or not existed. It returns true / false.
              Ex: sqr5.hasOwnProperty("width"); // false
           
-        B. 
-        ----------------------------------
-          *  
+          
+        B. Don't Overwrite Built in Prototypes:
+        --------------------------------------
+          *  We should not overwrite or change the Built in prototype. Like: Array.prototype.hudai = 
+             function () { }. It works. But it's harmful for the project. We should change 
+             our own Prototypes, not Built in Prototypes.
 
 
         C.
@@ -78,5 +81,12 @@ sqr5.hasOwnProperty("width"); // false
 sqr5.hasOwnProperty("height"); // true
 sqr5.hasOwnProperty("toString"); // false; Bcz, it's a part of prototype.
 sqr5.hasOwnProperty("getHeight"); // true
+
+// -----------------------------------------------------------------------------<<
+
+// Don't Overwrite Built in Prototypes
+Array.prototype.hudai = function () {}; // Don't do this
+
+Array.prototype.ownPrototype = function () {}; // Do this
 
 // -----------------------------------------------------------------------------<<
