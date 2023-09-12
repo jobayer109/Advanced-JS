e7759332a5518c343a171d574b234831dbf1180d
user.email <jobayer.ahmed109@gmail.com>
2023-09-12 06:31:04 +0600

    Polymorphism definition noted

---
+++ OOP/Pillars/Polymorphism/Polymorphism.js
@@ -0,0 +1,33 @@
+/*
+    Topics:
+    ----------------------------------------
+        A. Polymorphism
+        B. 
+        C. 
+        D. 
+        E. 
+       
+
+
+    Notes:
+    ------
+        A. Polymorphism:
+        ----------------
+          * The term Polymorphism is derived from the word “Polymorph,” where “Poly” 
+            means “Many” and “Morph” means “Transforming one form into another“. In 
+            Object-Oriented Programming, Polymorphism allows you to perform the same 
+            operation in multiple ways. It enables you to invoke the same method 
+            with different JavaScript objects by passing selected data members.
+          
+          
+        B. 
+        --------------------------------------
+          * 
+
+        C.
+        ---------------------------------
+          * 
+         
+*/
+
+// ------------------------------------------------------------------------->>
