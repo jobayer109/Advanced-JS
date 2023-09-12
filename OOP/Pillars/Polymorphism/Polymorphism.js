ecc84f0d17cc35fc242b4365d27ad64bd87e4344
user.email <jobayer.ahmed109@gmail.com>
2023-09-12 06:32:40 +0600

    Explore polymorphism with example

--- OOP/Pillars/Polymorphism/Polymorphism.js
+++ OOP/Pillars/Polymorphism/Polymorphism.js
@@ -31,3 +31,60 @@
 */
 
 // ------------------------------------------------------------------------->>
+// Common function to avoid code duplication to make a obj inherited.
+function extend(Parent, Child) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
+// Parent function
+const Shape = function (color) {
+  this.color = color;
+};
+Shape.prototype = {
+  common: function () {
+    console.log("This common method");
+  },
+};
+
+// Prototypical Inheritance of Square: Child function.
+const Sqr = function (width, color) {
+  Shape.call(this, color); // Super calling
+  this.width = width;
+  console.log("I am initial Draw");
+};
+// Best practice. call by extend function.
+extend(Shape, Sqr);
+
+//  Method override of sqr
+Sqr.prototype.common = function () {
+  console.log("I am square and I'm overriding function");
+};
+
+Sqr.prototype.draw = function () {
+  console.log("Draw function executed");
+};
+
+// Prototypical Inheritance of Circle
+const Circle = function (radius, color) {
+  this.radius = radius;
+  Shape.call(this, color);
+  console.log("I am initial Circle");
+};
+// Best practice. call by extend function.
+extend(Shape, Circle);
+
+// Method Overriding and executing Parent class's function.
+Circle.prototype.common = function () {
+  console.log("I am circle and I'm overriding function");
+};
+Circle.prototype.draw = function () {
+  console.log("Circle function executed");
+};
+
+// Output
+const sqr = new Sqr(10, "green");
+const circle = new Circle(10, "red");
+const shape = new Shape("Indigo");
+
+//------------------------------------<<
