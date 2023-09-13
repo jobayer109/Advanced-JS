1630a7425e088cd6c5675205e22154f33d55bdd5
user.email <jobayer.ahmed109@gmail.com>
2023-09-13 07:08:34 +0600

    Applied spread operator to create brand new something

--- ES6/Topics/Rest_&_Spread.js
+++ ES6/Topics/Rest_&_Spread.js
@@ -12,11 +12,15 @@
             * When use three dots (...) in function parameter, then it called rest.
             * Write (...rest) operator at the last of all parameters 
               Example: (name, msg, ...rest).
+            * Previously, we use arguments keyword. but it's not a array. 
+            * But when we use Rest (...rest) operator, it will convert all arguments 
+              to an array. And we can apply all array method like others.
            
            
         B. Spread operator:
         -------------------
-            * 
+            * It breaks the group of value to a single value.
+            * It creates something brand new like array, object.  
 */
 
 //----------------------------------------------------------------------->>
@@ -42,4 +46,32 @@ const getSum2 = (...rest) => {
 };
 getSum2(1, 2, 3, 2); // 8
 
+// Another best and simple approach -------------------
+const anotherSum = (...rest) => {
+  return rest.reduce((a, b) => a + b); // Reduce method
+};
+anotherSum(1, 2, 3); // 6
+
 //----------------------------------------------------------------------->>
+
+// B. Spread operator -------------------
+const arr = [1, 2, 3, 4, 5, 6];
+// console.log(...arr); // Single value: 1 2 3 4 5 6
+const arr2 = [...arr];
+arr2; // [1, 2, 3, 4, 5, 6];
+
+// --------------------------------------
+
+const obj = {
+  a: 10,
+  b: 56,
+  c: 65,
+};
+// Copy of reference
+const obj2 = obj; // Copy of reference
+console.log(obj2); // {a: 10, b: 56, c: 65}
+
+// Spread operator. Genuine copy.
+const obj3 = { ...obj };
+obj3.d = 12;
+console.log(obj3); // {a: 10, b: 56, c: 65, d: 12}
