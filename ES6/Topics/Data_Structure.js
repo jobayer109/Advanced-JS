185f1892cb8f7d80b6b35c2deb3699d22de95414
user.email <jobayer.ahmed109@gmail.com>
2023-09-15 19:56:31 +0600

    Data structure "map" explored. this so cool.

---
+++ ES6/Topics/Data_Structure.js
@@ -0,0 +1,73 @@
+/*
+    Topics:
+    -------
+        A. Data structure info
+        B. Set (data structure)  
+        C. Map (data structure)
+        
+
+    Notes:
+    -------
+        A. Data structure info:
+        ---------------------------
+            * Using Set and Map, we can convert an array to an object.
+            * Creating Map and Set constructor, we can use that as like as object 
+              related operations. 
+            * This is an advanced topic.
+           
+           
+        B. Set (data structure):
+        ---------------------------
+            * Set is a constructor.
+            * It doesn't allow duplicate elements.
+            * it is an object.
+            * we can add element with add() method from outside as like object.
+        
+        C. Map (data structure):
+        ---------------------------
+            * As same as Set, but there are some differences between them.
+            * In the structure, should say the "values" and "keys" in an array 
+              inside the parent array.
+            * Can be set any kinds of primitive data and objects.
+            * To set anything there is set() method, instead of add() method.
+            * for-of loop is something exceptional. Be aware about that.
+*/
+
+//----------------------------------------------------------------------->>
+
+// A. Set (data structure) -------------------
+const set = new Set([1, 2, 5, 3]);
+
+// Add element
+set.add(10);
+set.add(5); // Didn't add. Bcz, 5 a duplicate element
+
+// Some methods of set
+set.has(6); // false
+set.delete(6); // 6 isn't in here. so, it returns "false". If has, it'll delete.
+// console.log(set.entries(5));
+
+//----------------------------------------------------------------------->>
+
+// A.Map (data structure) -------------------
+const map = new Map([
+  ["a", 10],
+  ["b", 12],
+  ["c", 15],
+]);
+
+map; // Map(3) { 'a' => 10, 'b' => 12, 'c' => 15 }. It's an object
+
+// set() method
+map.set("f", 10);
+map.forEach((v, k) => {
+  v, k; // Returns key value pairs
+});
+
+// for-of loop in map.
+for (let [k, v] of map) {
+  k, v; // Returns key value pairs
+}
+// There are more useful methods in map data structure
+
+//----------------------------------------------------------------------->>
