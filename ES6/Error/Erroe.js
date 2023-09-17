4e83c60eacf7b682ea25499c60ab66c39b01242b
user.email <jobayer.ahmed109@gmail.com>
2023-09-17 14:18:57 +0600

    Custom Error handling with 'if-else'

--- ES6/Error/Erroe.js
+++ ES6/Error/Erroe.js
@@ -2,7 +2,7 @@
     Topics:
     -------
         A. Error definition
-        B. 
+        B. Error handling with 'if-else'
         C. 
 
 
@@ -18,5 +18,23 @@
 // A. Error definition:
 let n = 10;
 if (n > 5) {
-  throw new Error("error");
+  //   throw new Error("error");
 }
+
+//----------------------------------------------------------------------->>
+
+// B. Error handling with 'if-else'
+
+function parseToInt(v) {
+  const result = Number.parseInt(v);
+
+  if (!result) {
+    return "Please provide a valid number";
+  }
+  return result;
+}
+
+const output1 = parseToInt("45.65h"); //  45
+const output2 = parseToInt("t45.65h"); // Error message
+const output3 = parseToInt("Jobayer"); // Error message
+console.log(output3);
