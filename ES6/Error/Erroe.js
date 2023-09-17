818ae3d78fbff27baa5321273973246db08463b2
user.email <jobayer.ahmed109@gmail.com>
2023-09-17 15:19:55 +0600

    Error handling with 'try-catch' and  finally.

--- ES6/Error/Erroe.js
+++ ES6/Error/Erroe.js
@@ -3,7 +3,7 @@
     -------
         A. Error definition
         B. Error handling with 'if-else'
-        C. 
+        C. Error handling with 'try-catch'
 
 
     Notes:
@@ -37,4 +37,21 @@ function parseToInt(v) {
 const output1 = parseToInt("45.65h"); //  45
 const output2 = parseToInt("t45.65h"); // Error message
 const output3 = parseToInt("Jobayer"); // Error message
-console.log(output3);
+// console.log(output3);
+
+//----------------------------------------------------------------------->>
+
+// C. Error handling with 'try-catch' and  finally.
+function tryCatch() {
+  try {
+    console.log("I am line 1");
+    throw new Error("Ha ha ha, I'm Error");
+    console.log("I am line 2");
+    console.log("I am line 3");
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    console.log("I am final output");
+  }
+}
+// tryCatch();
