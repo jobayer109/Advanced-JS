a0af588e6336fd4d070a5812fad8362e69981fb2
user.email <jobayer.ahmed109@gmail.com>
2023-09-19 20:44:43 +0600

    AJAX with callback function (Bad practice)

--- ES6/Async/Ajax.js
+++ ES6/Async/Ajax.js
@@ -1,3 +1,23 @@
+/*
+    Topics:
+    -------
+        A. Simple AJAX
+        B. AJAX with callback function.(Bad & Good practice)
+
+
+
+
+
+
+
+
+
+*/
+
+//-------------------------------------------------------------------------->>
+
+// A. Simple AJAX
+
 function loadData() {
   const xtr = new XMLHttpRequest();
 
@@ -5,10 +25,28 @@ function loadData() {
     const result = xtr.responseText;
     // result.forEach((r) => console.log(r));
     const parsed = JSON.parse(result);
-    const response = parsed.forEach((r) => console.log(r.username));
+    const response = parsed.forEach((r) => r.username);
   };
   xtr.open("GET", "https://jsonplaceholder.typicode.com/users");
   xtr.send();
 }
 
-loadData();
+// loadData();
+
+//-------------------------------------------------------------------------->>
+
+// B. AJAX with callback function (Bad practice)
+
+const xhr = new XMLHttpRequest();
+xhr.open("get", "https://jsonplaceholder.typicode.com/users");
+
+xhr.onreadystatechange = function (e) {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const response = JSON.parse(xhr.response);
+    response.forEach((res) => console.log(res.username));
+  }
+};
+
+xhr.send();
+
+//-------------------------------------------------------------------------->>
