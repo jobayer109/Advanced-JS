4c55fd2f10fcb3b70511d8d0b13efeb6772a2838
user.email <jobayer.ahmed109@gmail.com>
2023-09-20 13:11:35 +0600

    Better promise exploration

--- ES6/Async/Promise.js
+++ ES6/Async/Promise.js
@@ -3,6 +3,7 @@
     -------
         A. What is promise?
         B. Simple promise.
+        C. Better promise exploration
 
     Notes:
     ------
@@ -24,3 +25,19 @@ p1.then((value) => {
 });
 
 //-------------------------------------------------------------------------->>
+
+// C. Better promise exploration
+function getIphone(isPassed) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      isPassed ? resolve("You got the Iphone") : reject(new Error("You are failed"));
+    }, 2000);
+  });
+}
+getIphone(false)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
