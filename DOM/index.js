0164827e682ce129bf99fd9c3aeef48824304a3c
user.email <jobayer.ahmed109@gmail.com>
2023-09-21 21:59:36 +0600

    Get document element by Query Selectors

--- DOM/index.js
+++ DOM/index.js
@@ -9,10 +9,16 @@
 
 */
 //----------------------------------------------------------------->>
-
+// Get element Selectors
 const title = document.getElementById("repeat");
 const classes = document.getElementsByClassName("text");
 const tagNames = document.getElementsByTagName("li");
 const nameOne = document.getElementsByName("list_one");
 
 //----------------------------------------------------------------->>
+
+// Query Selectors
+const title1 = document.querySelector("#repeat");
+const classes1 = document.querySelectorAll(".text");
+const tags = document.querySelectorAll("li");
+const name = document.querySelector("[name='list_one']");
