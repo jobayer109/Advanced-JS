677a5624849dc42906ecfa9eb7b7d6fa4b431b00
user.email <jobayer.ahmed109@gmail.com>
2023-09-21 21:30:29 +0600

    Get element by ID,class, Tag and name are explored

---
+++ DOM/index.js
@@ -0,0 +1,18 @@
+/*
+    Topics:
+    --------
+        1. Selectors
+            * getElementById
+            * getElementsByClassName
+            * getElementsByTagName
+            * getElementsByName
+
+*/
+//----------------------------------------------------------------->>
+
+const title = document.getElementById("repeat");
+const classes = document.getElementsByClassName("text");
+const tagNames = document.getElementsByTagName("li");
+const nameOne = document.getElementsByName("list_one");
+
+//----------------------------------------------------------------->>
