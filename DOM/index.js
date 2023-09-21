8fa48e5c93bd74809fb0699e2b937c4c831291e4
user.email <jobayer.ahmed109@gmail.com>
2023-09-21 22:29:22 +0600

    Explored Difference between "getElementBySomething" and "querySelector"

--- DOM/index.js
+++ DOM/index.js
@@ -6,7 +6,14 @@
             * getElementsByClassName
             * getElementsByTagName
             * getElementsByName
+            * querySelector_____________(important) 
+            * querySelectorAll__________(important) 
 
+        2. Difference between "getElementBySomething" and "querySelector"
+            * getElementBySomething shows the 'HTMLCollection' / Element node.
+            * querySelector shows the 'NodeList'. Kinds of nodes can be anything 
+              like "Elements, TagNames, Text, Attributes, names etc"
+            * querySelector is more powerful than getElementBySomething 
 */
 //----------------------------------------------------------------->>
 // Get element Selectors
@@ -22,3 +29,25 @@ const title1 = document.querySelector("#repeat");
 const classes1 = document.querySelectorAll(".text");
 const tags = document.querySelectorAll("li");
 const name = document.querySelector("[name='list_one']");
+
+//----------------------------------------------------------------->>
+
+// Difference between "getElementBySomething" and "querySelector"
+
+const li1 = document.getElementsByTagName("li");
+const li2 = document.querySelectorAll("li");
+
+// Simply console.log() output_________
+li1; // HTMLCollection(3)
+li2; // NodeList(3)
+
+// Check constructor___________________
+li1.constructor.name; // HTMLCollection
+li2.constructor.name; // NodeList
+
+// Check "ul" tag matters (children vs childNodes)
+const ul = document.querySelector("ul");
+ul.children; // HTMLCollection(3)
+ul.childNodes; // NodeList(7)
+
+//----------------------------------------------------------------->>
