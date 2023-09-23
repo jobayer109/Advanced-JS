96d8ad3418bda63bdab9325eb63f70ba08d53683
user.email <jobayer.ahmed109@gmail.com>
2023-09-23 14:15:44 +0600

    Mouse hover event explored and box scaling learned

--- DOM/Manipulation/Event.js
+++ DOM/Manipulation/Event.js
@@ -30,3 +30,24 @@ btn.addEventListener("click", function (e) {
   lastChild.innerHTML = "Newly added";
   list.appendChild(lastChild);
 });
+
+//------------------------------------------------------------------->>
+
+// B. Mouse hover event___________
+const box = document.getElementById("box");
+
+box.addEventListener("mousemove", function (e) {
+  // Global scaling
+  // document.getElementById("x-value").innerHTML = e.clientX;
+  // document.getElementById("y-value").innerHTML = e.clientY;
+
+  // Exact scaling of the particular box area.
+  document.getElementById("x-value").innerHTML = e.offsetX;
+  document.getElementById("y-value").innerHTML = e.offsetY;
+
+  //
+  // document.getElementById("x-value").innerHTML = e.pageX;
+  // document.getElementById("y-value").innerHTML = e.pageY;
+});
+
+//------------------------------------------------------------------->>
