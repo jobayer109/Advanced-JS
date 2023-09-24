30a718d5bba6ec53de599f54fdad9ada8098ad78
user.email <jobayer.ahmed109@gmail.com>
2023-09-24 15:27:49 +0600

    Get Form data and store in an object.

--- DOM/Manipulation/Form.js
+++ DOM/Manipulation/Form.js
@@ -5,3 +5,20 @@
 */
 
 //------------------------------------------------------------------->>
+
+// A. Get Form data and store in an object.
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let formData = {};
+  [...this.elements].forEach((el) => {
+    if (el.type !== "submit" && el.value !== "") {
+      formData[el.name] = el.value;
+    }
+  });
+  this.reset();
+  console.log(formData);
+});
