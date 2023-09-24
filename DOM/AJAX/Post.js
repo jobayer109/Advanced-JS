f148c9d58a5552bcf3bccaf1b88c7261e9a9e00c
user.email <jobayer.ahmed109@gmail.com>
2023-09-24 22:03:12 +0600

    By getting server response, display data in the UI

--- DOM/AJAX/Post.js
+++ DOM/AJAX/Post.js
@@ -1,13 +1,13 @@
 const form = document.getElementById("post-form");
-const post = document.getElementById("post");
+const posts = document.getElementById("posts");
 
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const title = this.title.value;
-  const body = this.body.value;
+  const title = this.title.value || undefined;
+  const body = this.body.value || undefined;
 
   if (title && body) {
     const formObj = {
@@ -25,8 +25,20 @@ form.addEventListener("submit", function (e) {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        const listItem = listItemGenerator(data);
+        posts.appendChild(listItem);
+
+        this.reset();
       })
       .catch((e) => console.log(e.message));
   }
 });
+
+function listItemGenerator(item) {
+  const li = document.createElement("li");
+  li.className = "list-group-item";
+  li.innerHTML = `${item.id}. "${item.title}" is posted by userID: ${item.userId}`;
+
+  return li;
+}
