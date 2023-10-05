5edde58f47d0607d345fc84955874d9c833a6d4f
user.email <jobayer.ahmed109@gmail.com>
2023-10-05 21:38:34 +0600

    Update array element using find and findIndex method

---
+++ Full-stack-Army/Array/Update.js
@@ -0,0 +1,35 @@
+/**
+      1. using findIndex.
+      2. using find method. And find() method is mutable. It will change 
+         after traversing
+ */
+
+const arr = [
+  { id: 1, value: 10 },
+  { id: 2, value: 20 },
+  { id: 3, value: 30 },
+  { id: 4, value: 40 },
+];
+
+// const index = arr.findIndex((v) => {
+//   return v.id === 2;
+// });
+// arr[1].value = 500;
+// console.log(arr);
+
+const obj = arr.find((v) => {
+  return v.id === 4;
+});
+
+obj.value = 400;
+// console.log(arr);
+
+arr[3] === obj; // true
+
+const a = {};
+const b = {};
+
+const c = a;
+
+a === b; // false; Bcz, They are representing diff ref
+a === c; // true; Bcz, c hold the a as reference.
