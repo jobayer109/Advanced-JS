6f054875b4a51c37be22a870d1481894bdf74c67
user.email <jobayer.ahmed109@gmail.com>
2023-10-07 21:35:33 +0600

    Compare the compiling time of array and object

---
+++ Full-stack-Army/Array-vs-Object/ArrayVsObject.js
@@ -0,0 +1,33 @@
+const arr = [];
+const obj = {};
+
+for (let i = 0; i < 5000000; i++) {
+  let o = {
+    id: i,
+    value: i,
+  };
+  arr.push(o);
+
+  obj[i] = o;
+}
+
+// ---------------------------------------------------------
+// Array time complexity test.
+console.time("Array time");
+const targetID = 4999998;
+const getObj = arr.find((item) => item.id === targetID);
+getObj.value = 555;
+console.log(getObj);
+console.timeEnd("Array time");
+
+// ---------------------------------------------------------
+// Object time complexity test.
+console.time("Object time");
+obj[targetID].value = 999;
+console.timeEnd("Object time");
+
+// Compiling__________________________________Result:
+/*
+    Array time: 73.246ms
+    Object time: 0.009ms
+*/
