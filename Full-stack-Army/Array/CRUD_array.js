4e623f7ca727afe422b29ddb054e85afc1468a71
user.email <jobayer.ahmed109@gmail.com>
2023-10-07 13:30:58 +0600

    Listing Advantage and disadvantage of using array

--- Full-stack-Army/Array/CRUD_array.js
+++ Full-stack-Army/Array/CRUD_array.js
@@ -8,12 +8,18 @@
 
 */
 
+/* 
+    Advantage and disadvantage of using array
+        - Easily Traverse
+        - Filter
+        - Delete
+        - Update (medium)
+        - Create a new one.
+            [ push -> easy O(1), unshift -> expensive task; O(n) ]
+
+*/
+
 // Random uuid generate
-/**
- * Returns a UUIDv4 as string
- *
- * @returns {string}
- */
 randomUuid = () => {
   return String("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx").replace(
     /[xy]/g,
@@ -25,3 +31,26 @@ randomUuid = () => {
     }
   );
 };
+
+// Create students array
+const students = [
+  {
+    id: "fa30d7de-7164-41c1-9274-6470da058458",
+    name: "Jobayer",
+    email: "jobayer@gmail.com",
+  },
+  {
+    id: "cf3b822f-239d-4443-a923-6eef71821f2d",
+    name: "Taima",
+    email: "@gmail.com",
+  },
+  {
+    id: "597d4662-4f91-4225-b65d-5bc09c95c0bc",
+    name: "Jara",
+    email: "jara@gmail.com",
+  },
+];
+
+// Create an
+
+console.log(students);
