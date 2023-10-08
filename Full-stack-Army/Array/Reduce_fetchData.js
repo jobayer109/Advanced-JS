6b6ce4808505f6b223bdfab9eceab2449b1f6807
user.email <jobayer.ahmed109@gmail.com>
2023-10-08 21:53:11 +0600

    data fetched by reduce method

--- Full-stack-Army/Array/Reduce_fetchData.js
+++ Full-stack-Army/Array/Reduce_fetchData.js
@@ -1,3 +1,18 @@
 const axios = require("axios").default;
 
 const url = "https://jsonplaceholder.typicode.com/posts";
+
+async function getData() {
+  const { data } = await axios.get(url);
+
+  const result = data.slice(0, 10).reduce((acc, cur) => {
+    acc[cur.id] = { ...cur };
+    delete acc[cur.id].body;
+    return acc;
+  }, {});
+  return result;
+}
+
+getData()
+  .then((data) => console.log(data))
+  .catch((e) => console.log(e.message));
