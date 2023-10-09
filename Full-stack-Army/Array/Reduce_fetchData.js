be8c472ff790ac71114b8171b4ecf567975fc1ac
user.email <jobayer.ahmed109@gmail.com>
2023-10-09 06:38:08 +0600

    Fetched data manage using map and compare with reduce

--- Full-stack-Army/Array/Reduce_fetchData.js
+++ Full-stack-Army/Array/Reduce_fetchData.js
@@ -5,6 +5,17 @@ const url = "https://jsonplaceholder.typicode.com/posts";
 async function getData() {
   const { data } = await axios.get(url);
 
+  // console.time("map");
+  // const res = data.slice(0, 10).map((post) => {
+  //   return {
+  //     id: post.id,
+  //     userId: post.userId,
+  //     title: post.title,
+  //   };
+  // });
+  // console.timeEnd("map");
+  // return res;
+
   const result = data.slice(0, 10).reduce((acc, cur) => {
     acc[cur.id] = { ...cur };
     delete acc[cur.id].body;
