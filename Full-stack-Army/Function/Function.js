02769c7411ef6d27d63ac9a69e8122865ed3dbe0
user.email <jobayer.ahmed109@gmail.com>
2023-10-10 19:33:25 +0600

    arrow function

--- Full-stack-Army/Function/Function.js
+++ Full-stack-Army/Function/Function.js
@@ -6,13 +6,18 @@ const result = add(2, 3); // Calls the add function and stores the result in 're
 console.log(result); // Outputs 5
 
 // --------------------------------------------------------
+// Arrow Functions:
+const multiply = (a, b) => a * b;
+console.log(multiply(4, 5)); // Outputs 20
+
+// --------------------------------------------------------
 // Higher-Order Functions:
 function operate(num1, num2, operation) {
   return operation(num1, num2);
 }
 
 const add = (a, b) => a + b;
-const multiply = (a, b) => a * b;
+const multiply1 = (a, b) => a * b;
 
 console.log(operate(2, 3, add)); // Outputs 5
-console.log(operate(2, 3, multiply)); // Outputs 6
+console.log(operate(2, 3, multiply1)); // Outputs 6
