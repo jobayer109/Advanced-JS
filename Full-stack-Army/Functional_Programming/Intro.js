dfb6c77e8fab6bb53381f0221633041ac2f589c6
user.email <jobayer.ahmed109@gmail.com>
2023-10-13 12:20:29 +0600

    Pure function and side effect explored

--- Full-stack-Army/Functional_Programming/Intro.js
+++ Full-stack-Army/Functional_Programming/Intro.js
@@ -1,6 +1,6 @@
 /*
     Topics of Functional Programming
-        1. Pure function
+        1. Pure function + Side effect
         2. Higher order function
         3. Function scope
         4. Closure
@@ -8,3 +8,20 @@
         6. Callback function
         7. Instant Invoked Functional Expression (IIFE)
  */
+
+// ---------------------------------------------------------------
+
+//   1. Pure function
+function sum(a, b) {
+  return a + b;
+}
+sum(20, 10); // 30
+
+// 1.1: Side effect
+let limit = 100;
+function changeLimit(limit) {
+  limit = 500;
+  return limit;
+}
+changeLimit(limit); // nothing
+console.log(changeLimit(limit)); // 500
