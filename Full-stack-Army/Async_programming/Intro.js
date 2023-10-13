9b264e3ea24c8312c8907d848cfbbe065033e777
user.email <jobayer.ahmed109@gmail.com>
2023-10-13 21:43:04 +0600

    SImple async explored

---
+++ Full-stack-Army/Async_programming/Intro.js
@@ -0,0 +1,27 @@
+/*
+    Topics:
+    ------------------------------
+        1.
+        2.
+        3.
+
+    Description:
+    ------------------------------
+
+*/
+
+// --------------------------------------------------------------
+
+console.log("first line 1");
+
+setTimeout(() => {
+  console.log("first line 2");
+}, 2000);
+
+console.log("first line 3");
+
+setTimeout(() => {
+  console.log("first line 4");
+}, 1000);
+
+console.log("last line 5");
