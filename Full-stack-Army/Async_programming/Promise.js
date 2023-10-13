60f84387dee8b58ea4892b337c414e5ffd2397c1
user.email <jobayer.ahmed109@gmail.com>
2023-10-13 23:05:58 +0600

    Make a timer using Promise (Interview Question)

--- Full-stack-Army/Async_programming/Promise.js
+++ Full-stack-Army/Async_programming/Promise.js
@@ -1,3 +1,12 @@
+/*
+    1. Simple Promise
+    2. Make a timer using Promise (Interview Question)
+    3.
+
+*/
+// -------------------------------------------------------------
+
+// 1. Simple Promise
 const isValid = false;
 const promise = new Promise((resolve, reject) => {
   if (isValid) {
@@ -14,3 +23,18 @@ promise
   .catch((e) => {
     console.log(e.message);
   });
+
+// -------------------------------------------------------------
+
+// 2. Make a timer using Promise (Interview Question)
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+wait(1000).then(() => {
+  console.log("Print after 1s");
+});
+wait(2000).then(() => {
+  console.log("Print after 2s");
+});
+wait(3000).then(() => {
+  console.log("Print after 3s");
+});
