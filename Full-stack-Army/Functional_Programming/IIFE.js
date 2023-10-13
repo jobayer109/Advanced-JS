c94652f4119d5507c6166a6a7939bb510442a68f
user.email <jobayer.ahmed109@gmail.com>
2023-10-13 20:10:46 +0600

    Simply explored IIFE

--- Full-stack-Army/Functional_Programming/IIFE.js
+++ Full-stack-Army/Functional_Programming/IIFE.js
@@ -1,3 +1,19 @@
+/*
+
+    IIFE stands for "Immediately Invoked Function Expression" in JavaScript. It is a design pattern used to create and execute a function immediately after its declaration.
+
+    Why IIFE:
+    -------------------
+    IIFE is commonly used **to create a private scope for variables, preventing them from polluting the global scope**. It is also used for tasks like **initializing variables, setting up configurations, or encapsulating code to avoid naming conflicts in complex applications.** It's a useful tool for maintaining clean and organized JavaScript code.
+
+*/
+
+// Arrow function
 (() => {
   console.log("Hello world");
+})();
+
+// Normal function
+(function myFn() {
+  console.log("Hello from IIFE");
 })();
