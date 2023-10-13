c95680f95fb0d58395c8194b0c17cb21cfa223d1
user.email <jobayer.ahmed109@gmail.com>
2023-10-13 12:29:23 +0600

    Impure and side effected function related with array

--- Full-stack-Army/Functional_Programming/Intro.js
+++ Full-stack-Army/Functional_Programming/Intro.js
@@ -25,3 +25,16 @@ function changeLimit(limit) {
 }
 changeLimit(limit); // nothing
 console.log(changeLimit(limit)); // 500
+
+// Pure function related with array
+// This is a pure function bcz, this function took the array as an argument as the function's normal behavior.
+const arr = [1, 2, 3];
+function change(arr, data) {
+  arr.push(data);
+}
+// Impure and side effected function related with array
+// This is a impure function bcz, this function change the array element outside of the function.
+const array = [1, 2, 3];
+function change(data) {
+  array.push(data);
+}
