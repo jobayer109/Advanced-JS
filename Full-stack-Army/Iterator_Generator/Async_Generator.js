50c7ac6d74125a063fc9b0431b6819e0e0846660
user.email <jobayer.ahmed109@gmail.com>
2023-10-14 14:22:57 +0600

    explored async Generator and play with JSON data

--- Full-stack-Army/Iterator_Generator/Async_Generator.js
+++ Full-stack-Army/Iterator_Generator/Async_Generator.js
@@ -1,27 +1,27 @@
-/*
-    Topics:
-    ---------------------------------
-        A. 
-        B. 
-        C. 
-         
+const axios = require("axios").default;
 
-    Notes:
-    ---------------------------------
-      * 
-      
-*/
+async function getUsers() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const { data: users } = await axios.get(url);
+  return users;
+}
 
-// ------------------------------------------------------------------
+async function* getPostByUser(users) {
+  const url = "https://jsonplaceholder.typicode.com/posts";
 
-//
+  for (let i = 0; i < users.length; i++) {
+    const { data: posts } = await axios.get(`${url}?userId=${users[i].id}`);
+    yield posts;
+  }
+}
 
-// ------------------------------------------------------------------
-
-//
-
-// ------------------------------------------------------------------
-
-//
-
-// ------------------------------------------------------------------
+getUsers()
+  .then(async (users) => {
+    const userIterator = getPostByUser(users);
+    (await userIterator.next()).value; // userId: 1
+    (await userIterator.next()).value; // userId: 2
+    (await userIterator.next()).value; // userId: 3
+  })
+  .catch((e) => {
+    console.log(e);
+  });
